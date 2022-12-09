4e2b894cf2c32c2252154913ed836e2bdb9b42be
csikie <58120619+csikie@users.noreply.github.com>
2022-12-09 19:01:41 +0100

    day 1

--- .net/day1/Program.cs
+++ .net/day1/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt");
+
+List<int> partOne(string[] input)
+{
+    int max = 0;
+    int temp = 0;
+    var calSums = new List<int>();
+    foreach (var item in input)
+    {
+        if (string.IsNullOrWhiteSpace(item))
+        {
+            max = temp > max ? temp : max;
+            calSums.Add(temp);
+            temp = 0;
+        }
+        else
+        {
+            temp += int.Parse(item);  
+        }  
+    }
+    Console.WriteLine(max);
+    return calSums;
+}
+
+void partTwo()
+{
+    var calSums = partOne(input);
+    calSums.Sort((a, b) => b.CompareTo(a));
+    Console.WriteLine($"{calSums[0]+calSums[1]+calSums[2]}");
+}
+
+partTwo();
